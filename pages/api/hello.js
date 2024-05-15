d9e0555d05acdde3d2e2f42882e2021b1366d7f1
Clement Berard <clement.berard@ecole-isitech.fr>
2024-05-15 14:05:10 +0200

    add api call

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function recupCitation(req, res) {
+  const url = "https://kaamelott.chaudie.re/api/random";
+  try {
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    res.status(200).json(data);
+  } catch (erreur) {
+    console.error("Erreur lors de la requête : ", erreur);
+    res.status(500).json({ message: erreur });
+  }
 }
